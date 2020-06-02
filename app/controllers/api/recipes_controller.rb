e52c6501545d39a10f359990ba7e995acc0ff058
Jeneen <jeneen.lemar@gmail.com>
2020-06-02 04:40:00 -0400

    adds restful routes

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -1,7 +1,50 @@
 class Api::RecipesController < ApplicationController
 
-def one_recipe_action
-  @recipe = Recipe.first
-  render "one_recipe.json.jb"
+# def one_recipe_action
+#   @recipe = Recipe.first
+#   render "one_recipe.json.jb"
+# end
+
+
+def index
+  @recipes = Recipe.all
+  render "index.json.jb"
+end
+
+def show
+  @recipe = Recipe.find_by(id: params[:id])
+  render "show.json.jb"
+end
+
+def create
+  @recipe = Recipe.new(
+    title: params[:title],
+    ingredients: params[:ingredients] ,
+    directions: params[:directions],
+    prep_time: params[:prep_time],
+    image_url: params[:image_url],
+    chef: params[:chef]
+  )
+  @recipe.save
+  render "show.json.jb"
+  
+end
+
+def update
+  @recipe = Recipe.find_by(id: params[:id])
+  @recipe.title = params[:title] || @recipe.title
+  @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+  @recipe.directions = params[:directions] || @recipe.directions
+  @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+  @recipe.chef = params[:chef] || @recipe.chef
+  @recipe.image_url = params[:image_url] || @recipe.image_url
+
+  render "show.json.jb"
+end
+
+def destroy
+  @recipe = Recipe.find_by(id: params[:id])
+  render json: {message: "Recipe destroyed successfully"}
+  @recipe.destroy
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/one_recipe_url" => "recipes#one_recipe_action"
+    get "/recipes" => "recipes#index"
+    get "/recipes/:id" => "recipes#show"
+    post "/recipes" => "recipes#create"
+    patch "/recipes/:id" => "recipes#update"
+    delete "/recipes/:id" => "recipes#destroy"
   end
 end
